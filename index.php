5c52556ee6306fc2f0b1b0122ba88912e60191a0
Elvis Detona <sergiowebdesigner@gmail.com>
2013-02-28 19:15:42 -0300

    estrutura home

--- index.php
+++ index.php
@@ -6,49 +6,50 @@ $itens = array(
 )
 ?>
 <!DOCTYPE HTML>
-<html lang="en-US">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <title>Ulalá</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body class="wrapper">
+    <header role="banner">
+        <h1 class="logo ir">Prêmio Sebrae Mulher de Negócios</h1>
+        <nav role="navigation">
+            <ul>
+                <?php foreach ($itens as $k => $item): ?>
+                    <li <?php echo ($k == $page) ? 'class="current"' : "" ?>>
+                        <a href="?page=<?php echo $k ?>"><?php echo $item ?></a>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        </nav>
+    </header>
 
-    <div class="wrapper">
-        <header>
-            <h1 class="logo ir">Prêmio Sebrae Mulher de Negócios</h1>
-            <nav>
-                <ul>
-                    <?php foreach ($itens as $k => $item): ?>
-                        <li <?php echo ($k == $page) ? 'class="current"' : "" ?>>
-                            <a href="?page=<?php echo $k ?>"><?php echo $item ?></a>
-                        </li>
-                    <?php endforeach ?>
-                </ul>
-            </nav>
-        </header>
-
-        <div class="main">
-            <div class="container">
-                <div class="content">
-                    <p>string</p>
-                    <p>string</p>
-                    <p>string</p>
-                    <p>string</p>
-                    <p>string</p>
-                    <p>string</p>
-                    <p>string</p>
-                    <p>string</p>
-                    <p>string</p>
-                    <p>string</p>
-                    <p>lulu</p>
-                </div>
+    <main role="main" class="main">
+        <div class="container">
+            <div class="content">
+                <p>string</p>
+                <p>string</p>
+                <p>string</p>
+                <p>string</p>
+                <p>string</p>
+                <p>string</p>
+                <p>string</p>
+                <p>string</p>
+                <p>string</p>
+                <p>string</p>
+                <p>lulu</p>
             </div>
-
-            <a href="#" id="up">Up</a>
-            <a href="#" id="down">down</a>
         </div>
-    </div>
+
+        <a href="#" id="up">Up</a>
+        <a href="#" id="down">down</a>
+    </main>
+
+    <footer role="contentinfo">
+        
+    </footer>
     <!-- jQuery -->
     <script src="js/vendor/jquery/jquery.js" type="text/javascript"></script>
 
