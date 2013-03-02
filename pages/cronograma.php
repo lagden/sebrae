be001b476a7923a0d1a1d31ef3a7eccead3cee9e
Elvis Detona <sergiowebdesigner@gmail.com>
2013-03-01 23:10:46 -0300

    vai

---
+++ pages/cronograma.php
@@ -0,0 +1,35 @@
+<div class="container">
+    <h2>CRONOGRAMA</h2>
+    <h3>
+        Sua trajetória pode abrir caminho<br />
+        para milhares de outras mulheres
+    </h3> 
+    
+    <div class="box-cronograma">
+        <table>
+            <tr>
+                <td rowspan="3">Vai Corinthians</td>
+                <td class="roxo" data-dia="07 março">07 março</td>
+                <td class="roxo" data-dia="31 julho">31 julho</td>
+                <td class="roxo" data-dia="agosto/outubro">agosto/outubro</td>
+            </tr>
+            <tr>
+                <td class="rosa" data-dia="outubro/fevereiro">outubro/fevereiro</td>
+                <td class="rosa" data-dia="18 novembro">18 novembro</td>
+                <td class="cinza" data-dia="/">/</td>
+
+            </tr>
+            <tr>
+                <td class="vermelho" data-dia="outubro/fevereiro">outubro/fevereiro</td>
+
+
+            </tr>
+        </table>
+    </div>
+
+</div>
+
+
+
+
+
